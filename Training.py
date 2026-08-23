cfaaf3fec04d3b44978148c9e4baa898df26c021
Tyler Williams <thyak123@gmail.com>
2026-08-23 17:38:28 -0500

    moved training to cuda, added guard against working recursively creating processes, and added model saving.

--- Training.py
+++ Training.py
@@ -1,25 +1,37 @@
 import torch
-import torchvision.transforms as transforms
 import Network
 
-AlexNet = Network.AlexNet()
-optim = torch.optim.SGD(AlexNet.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005) #devide lr by 10 when the validation error rate stopps improving
-loss_fn = torch.nn.CrossEntropyLoss()
+def main():
+    device = torch.device("cuda")
 
-for i in range(5):
-    data = torch.load(f"Processed_Images_{i}.pt")
+    AlexNet = Network.AlexNet().to(device)
+    optim = torch.optim.SGD(AlexNet.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005) #devide lr by 10 when the validation error rate stopps improving
+    loss_fn = torch.nn.CrossEntropyLoss()
 
-    dataLoader = torch.utils.data.DataLoader(data, batch_size=128, num_workers=3)
+    for epoch in range(2):
+        for i in range(5):
+            data = torch.load(f"data/Processed_Images_{i}.pt")
 
-    for batch, (x, y) in enumerate(dataLoader):
-        x = x.float() / 255
+            dataLoader = torch.utils.data.DataLoader(data, batch_size=128, shuffle=True, num_workers=3)
 
-        y_hat = AlexNet(y)
-        loss = loss_fn(y_hat, y)
+            for batch, (x, y) in enumerate(dataLoader):
+                x = x.float() / 255
+                x = x.to(device)
+                y = y.to(device)
 
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
+                y_hat = AlexNet(x)
+                loss = loss_fn(y_hat, y)
 
-        if(batch % 50 == 0):
-            print("loss: " + loss)
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+
+                if(batch % 50 == 0):
+                    print("loss: ", loss.item())
+
+            del data
+
+    torch.save(AlexNet.state_dict(), "Model.pt")
+
+if __name__ == "__main__":
+    main()
