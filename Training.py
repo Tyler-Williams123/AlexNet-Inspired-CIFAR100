1f1889030800531263039b6375e2e23809572d29
Tyler Williams <thyak123@gmail.com>
2026-08-26 18:11:26 -0500

    refactored training to new dataset.

--- Training.py
+++ Training.py
@@ -8,29 +8,24 @@ def main():
     optim = torch.optim.SGD(AlexNet.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005) #devide lr by 10 when the validation error rate stopps improving
     loss_fn = torch.nn.CrossEntropyLoss().to(device)
 
+    images, targets = torch.load(f"data/Processed_Images.pt")
+    data = torch.utils.data.TensorDataset(images, targets)
+    dataLoader = torch.utils.data.DataLoader(data, batch_size=128, shuffle=True, num_workers=0, pin_memory=True)
+
     for epoch in range(10):
-        for i in range(5):
-            images, targets = torch.load(f"data/Processed_Images_{i}.pt")
-            data = torch.utils.data.TensorDataset(images, targets)
+        for batch, (x, y) in enumerate(dataLoader):
+            x = x.to(device)
+            y = y.to(device)
 
-            dataLoader = torch.utils.data.DataLoader(data, batch_size=128, shuffle=True, num_workers=0, pin_memory=True)
+            y_hat = AlexNet(x)
+            loss = loss_fn(y_hat, y)
 
-            for batch, (x, y) in enumerate(dataLoader):
-                x = x.float() / 255
-                x = x.to(device, non_blocking=True)
-                y = y.to(device, non_blocking=True)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
 
-                y_hat = AlexNet(x)
-                loss = loss_fn(y_hat, y)
-
-                optim.zero_grad()
-                loss.backward()
-                optim.step()
-
-                if(batch % 50 == 0):
-                    print("loss: ", loss.item())
-
-            del data
+            if(batch % 50 == 0):
+                print("loss: ", loss.item())
 
     torch.save(AlexNet.state_dict(), "Model.pt")
 
